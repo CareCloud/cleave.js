57b0c05e27cf796efcc2f51e5ebf1fb5604f3732
Andreas Kruse <andreas.kruse_externerMitarbeiter@provinzial.de>
2018-06-07 13:02:03 +0200

    add option alwaysShowDecimals

--- src/Cleave.js
+++ src/Cleave.js
@@ -54,7 +54,6 @@ Cleave.prototype = {
         owner.element.addEventListener('cut', owner.onCutListener);
         owner.element.addEventListener('copy', owner.onCopyListener);
 
-
         owner.initPhoneFormatter();
         owner.initDateFormatter();
         owner.initNumeralFormatter();
@@ -80,7 +79,8 @@ Cleave.prototype = {
             pps.numeralThousandsGroupStyle,
             pps.numeralPositiveOnly,
             pps.stripLeadingZeroes,
-            pps.delimiter
+            pps.delimiter,
+            pps.alwaysShowDecimals
         );
     },
 
